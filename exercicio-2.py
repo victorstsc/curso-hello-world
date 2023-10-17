8239dc3dc48adc1da3184d214abaeea3d771948a
victorstsc <victorsschirmer@gmail.com>
2023-10-17 19:56:20 -0300

    Create exercicio-2.py

---
+++ exercicio-2.py
@@ -0,0 +1,22 @@
+def senha1():
+    print("Digite a primeira senha!")
+    texto1 = input()
+
+    return texto1
+
+def senha2():
+    print("Digite a segunda senha!")
+    texto2 = input()
+
+    return texto2
+
+def senha3():
+    print("Digite a terceira senha!")
+    texto3 = input()
+
+    return texto3
+
+def parabens():
+    print("Parabéns!")
+
+
